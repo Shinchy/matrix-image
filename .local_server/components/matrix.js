44fe4c3b2708283c2fcab69b4a1ab7ece391f613
Shaun Hinchy <Shaun@Picto-Network.home>
2017-12-01 23:17:38 +0000

    updated it to work

--- .local_server/components/matrix.js
+++ .local_server/components/matrix.js
@@ -1,16 +1,17 @@
 /**
- * 
+ * Needs comments
  * 
  * Web Component for the matrix filter
  */
 
  'use strict';
 
+
  customElements.define('matrix-img', class extends HTMLElement {
 
 
     static get observedAttributes() {
-
+        return ['src'];
     }
 
     /**
@@ -18,22 +19,40 @@
      */
     constructor() {
         super();
+        //
+        const doc = document.currentScript.ownerDocument;
+        const tmpl = doc.querySelector('#matrix-img-tmpl');
+
+        this._root = this.attachShadow({ mode: 'open' });
+        this._root.appendChild(tmpl.content.cloneNode(true));
+        this._elementProxy = document.createElement('div');
     }
 
 
     connectedCallback() {
         // first setup the canvas and img
         var canvas = document.createElement('canvas');
-        var img = document.getElementById('test-image');
+        var img = document.createElement('img');
+        var container = this._root.querySelector('#matrix-img-content');
+        
+        this.src = this.getAttribute('src');
+        img.src = this.src;
+        img.onload = () => {
 
-        // Then allocate
-        canvas.width = img.width;
-        canvas.height = img.height;
-        canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
+            // Then allocate
+            if(img) {
+                canvas.width = img.width;
+                canvas.height = img.height;
+                canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
+            } else return;
 
-        // Then define
-        this.canvas = canvas;
-        this.img = img;
+            // Then define
+            this.canvas = canvas;
+            this.img = img;
+
+            if(container) container.innerHTML = this.getImageData();
+        };       
+        
     }
 
     disconnectedCallback() {
@@ -45,16 +64,36 @@
     }
 
 
+
     /**
      * Get the data from the image, pixel by pixel
      * 
      * @returns [Array]
      */
     getImageData() {
-        var pixelData = [];
+        var pixelData = "";
+        var scope = 5;
+        var range = 10;
+
         // go through the first 10 pixels
-        for(var i = 0; i < this.img.width; i++) {
-            pixelData.push(this.canvas.getContext('2d').getImageData(i, (1), 1, 1).data);
+        for(var i = 0; i < this.img.height; i += scope) {
+            var innerPoint = "";
+
+            for(var j = 0; j < this.img.width; j += scope) {
+                var imgData = this.canvas.getContext('2d').getImageData(j, i, 1, 1).data;
+                if( (imgData[0] < 255 - range || 
+                     imgData[1] < 255 - range || 
+                     imgData[1] < 255 - range) && 
+                    (imgData[0] > 0 + range || 
+                     imgData[1] > 0 + range || 
+                     imgData[1] < 0 + range)) {
+                    innerPoint += "0";
+                } else {
+                    innerPoint += "-";
+                }
+            }
+
+            pixelData += innerPoint + "<br>";
         }
 
         return pixelData;
